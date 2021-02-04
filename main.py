571ad746af91045660152fa425c5d80fdca09203
dawidkraszkiewicz <kraszka1234@gmail.com>
2021-02-04 16:34:57 +0100

    Beggining to create a name States of America game

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import csv
-import pandas
 
-with open("weather_data.csv") as csv_data:
-    data = csv.reader(csv_data)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
-
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
